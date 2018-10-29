cd26b6e03d37d02c12f7af158f32a477f44b876b
bkichler <brian.kichler@gmail.com>
2018-10-29 01:36:14 +0000

    Move is_obstructed? method to piece model, add and run migration to change row and column names for piece to x and y

--- app/models/move.rb
+++ app/models/move.rb
@@ -2,7 +2,4 @@ class Move < ApplicationRecord
   belongs_to :game
   belongs_to :piece 
 
-  def is_obstructed?
-    
-  end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,4 +2,11 @@ class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
   has_many :moves
+
+
+
+  def is_obstructed?
+    
+    true
+  end
 end
