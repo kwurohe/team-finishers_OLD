500c138c2e124699ea7eac42dd36df1c4f9c3351
Philip Vuong <philipvuong@yahoo.com>
2018-10-31 22:48:52 +0000

    created game board, added scope for available games

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,6 @@ class Game < ApplicationRecord
   has_many :users
   has_many :pieces
   has_many :moves
+
+  scope :available, -> { where('black_player_user_id = null').or(where('white_player_user_id = null')) } 
 end
