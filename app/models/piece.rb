57ecadf897f95ea19d3f8836acbbab6d6fd5980e
Seth Brown <sethwbrown22@gmail.com>
2018-10-30 22:37:59 -0400

    A few fixes after testing - 1.Board Size 2.End condition Logic 3. Return Statement

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,8 +8,8 @@ class Piece < ApplicationRecord
   	# The first digit in the coordinate pair is the column (vertical) and the second digit is the row (horizontal)
 
     #check if move is off board
-    if column > 8 || column < 0 || row > 8 || row < 0
-      true
+    if column > 7 || column < 0 || row > 7 || row < 0
+      return true
     end
 
     #build an array of all the pieces in the current game
@@ -17,16 +17,16 @@ class Piece < ApplicationRecord
 
     #use the pieces array to build an array of all the occupied spaces in the game
     occupied_spaces = []
-    pieces.each do |piece|
-    	occupied_spaces << [piece.current_column, piece.current_row]
+    pieces.each do |p|
+    	occupied_spaces << [p.current_column, p.current_row]
     end 
 
     #build an array of the spaces in the move
     move_spaces = []
-    current_column = piece.column
-    current_row = piece.row
+    current_column = piece.current_column
+    current_row = piece.current_row
 
-    while current_column != column && current_row != row
+    while current_column != column || current_row != row
         if current_column - column < 0
           current_column += 1 #move right
         elsif current_column - column > 0
@@ -45,7 +45,6 @@ class Piece < ApplicationRecord
     #check whether any occupied spaces or spaces in move overlap (are common)
     overlap =  occupied_spaces & move_spaces
     overlap.length > 0 ? true : false
-
 
   end
 
