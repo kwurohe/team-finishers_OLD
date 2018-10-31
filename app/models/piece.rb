92438be935168366afd4f85cffc79fe55826ae52
Seth Brown <sethwbrown22@gmail.com>
2018-10-30 23:09:02 -0400

    Further comments on how code works, and what is left to do

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -6,22 +6,27 @@ class Piece < ApplicationRecord
   def is_obstructed?(piece, column, row)
   	# This method is designed to work within an 8x8 grid (standard chess board) where the bottom left corner is (0,0) and the top right corner is (8,8)
   	# The first digit in the coordinate pair is the column (vertical) and the second digit is the row (horizontal)
+  	# It takes a Piece object, as well as a column coordinate (x-axis) and a row cooridnate (y-axis)
+  	# It returns true if a move is obstructed between the start and end point 
+  	# It does NOT RETURN TRUE IF AN END POINT IS OBSTRUCTED
 
-    #check if move is off board
+    # Check if move is off board
     if column > 7 || column < 0 || row > 7 || row < 0
       return true
     end
 
-    #build an array of all the pieces in the current game
+    # TODO: Build special condition for Knight that always returns true
+
+    # Build an array of all the pieces in the current game
     pieces = game.pieces
 
-    #use the pieces array to build an array of all the occupied spaces in the game
+    # Use the pieces array to build an array of all the occupied spaces in the game
     occupied_spaces = []
     pieces.each do |p|
     	occupied_spaces << [p.current_column, p.current_row]
     end 
 
-    #build an array of the spaces in the move
+    # Build an array of the spaces in the move
     move_spaces = []
     current_column = piece.current_column
     current_row = piece.current_row
@@ -42,7 +47,10 @@ class Piece < ApplicationRecord
       move_spaces << [current_column, current_row]
     end
 
-    #check whether any occupied spaces or spaces in move overlap (are common)
+    # Removing the last element from move_spaces array. This last element would be the space you land on, so it doesn't matter if there is another piece there, you would just take it.
+		move_spaces.delete_at(move_spaces.length-1)
+
+    # Check whether any occupied spaces or spaces in move overlap (are common)
     overlap =  occupied_spaces & move_spaces
     overlap.length > 0 ? true : false
 
