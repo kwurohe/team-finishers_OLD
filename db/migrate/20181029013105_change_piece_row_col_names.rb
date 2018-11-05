9fa682d12ad3524aaed60f9d5933a735aa294313
Brian Kichler <brian.kichler@gmail.com>
2018-11-04 16:52:31 -0800

    Delete duplicate migration file

--- db/migrate/20181029013105_change_piece_row_col_names.rb
+++
@@ -1,6 +0,0 @@
-class ChangePieceRowColNames < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :pieces, :current_row, :x_pos
-    rename_column :pieces, :current_column, :y_pos
-  end
-end
