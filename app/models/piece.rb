650e5a656c76ce7dfdb70a1dfa04c68330f4fdc1
bkichler <brian.kichler@gmail.com>
2018-11-06 03:35:57 +0000

    Finish move_to! method and test in console.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -42,19 +42,20 @@ class Piece < ApplicationRecord
     raise "Invalid move" if move_direction == :invalid
   end
 
+  # move_to! method calls valid_move? and will update a piece instance's
+  # position and/or capture by setting positions to null. Could update
+  # this later to incorporate a piece status
+
   def move_to!(x_new, y_new)
-    if game.piece_present(x_new, y_new).nil? && valid_move?(x_new, y_new)
-      update_attributes(x_pos: x_new, y_pos: y_new)
+    return raise "Invalid move" if !valid_move?(x_new, y_new)
+    occupant = game.piece_present(x_new, y_new)
+    current_piece = game.pieces.where(x_pos: x_pos, y_pos: y_pos).first
+    if occupant.nil?
+      current_piece.update_attributes(x_pos: x_new, y_pos: y_new)
+    elsif occupant.color != current_piece.color
+      current_piece.update_attributes(x_pos: x_new, y_pos: y_new)
+      occupant.update_attributes(x_pos: nil, y_pos: nil)
+    else return raise "Invalid move"
     end
   end
 end
-# This method is important and eventually, we will use it in the controller to handle moving pieces.
-
-# This move_to method should handle the following cases:
-
-#     Check to see if there is a piece in the location it’s moving to.
-#     If there is a piece occupying the location, and it is the opposite color, remove the piece from the chess board. This can be done a few different ways.
-#         You could have a “status” flag on the piece that will be one of “onboard” or “captured”. Make sure to write the appropriate migration without changing existing migrations.
-#     If the piece is there and it’s the same color the move should fail - it should either raise an error message or do nothing.
-#     It should call update_attributes on the piece and change the piece’s x/y position.
-#     Note: This method does not check if a move is valid. We will be using the valid_move? method to do that.
